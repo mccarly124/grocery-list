f9c607722a5b72c47df6415afc15d113859d690a
Maggie <mccarly124@gmail.com>
2025-04-09 21:45:36 -0400

    Adding cors

--- src/main/java/com/food/grocerylist_part2/BasicController.java
+++ src/main/java/com/food/grocerylist_part2/BasicController.java
@@ -4,33 +4,24 @@ import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller // This means that this class is a Controller
-@RequestMapping(path="/list")
+@RestController // This means that this class is a Controller
+@RequestMapping(path="/grocery")
 public class BasicController {
 
     @Autowired
     private ItemRepository itemRepository;
 
-    @PostMapping(path="/add")
-    public @ResponseBody String addNewTransaction(@RequestParam String name, @RequestParam String store, @RequestParam String category, @RequestParam Float price, @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date date){
-        Item n = new Item();
-        n.setCategory(category);
-        n.setDate(date);
-        n.setName(name);
-        n.setPrice(price);
-        n.setStore(store);
-        itemRepository.save(n);
+    @CrossOrigin(origins = "http://localhost:5173")
+    @PostMapping(path="/post")
+    public @ResponseBody String addNewTransaction(@RequestBody Item item){
+        itemRepository.save(item);
         return "Added!";
     }
 
-    @GetMapping("/grocery")
+    @CrossOrigin(origins = "http://localhost:5173")
+    @GetMapping("/get")
     public @ResponseBody Iterable<Item> getAllUsers() {
         // This returns a JSON or XML with the users
         return itemRepository.findAll();
